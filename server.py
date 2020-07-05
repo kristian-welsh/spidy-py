6d4954ac0a90d25feacd3262df4a51142c98b390
Kristian Welsh <kriswelsh@gmail.com>
2020-07-05 21:49:04 +0100

    refactor and create spine of framework
    I have formalized the process of handing a request response pair
    First the request object is created,
    Then the apropriate responder is found by the router
    Next the responder creates a response
    (this is where the framework client will process the REST request)
    Finally, that response is converted to string and sent back through the socket
    Oh, and the connection is closed.

--- server.py
+++ server.py
@@ -2,43 +2,89 @@ import socket
 
 HOST = '127.0.0.1'
 PORT = 8080
-responseBody = """<html>
+RESPONSE_BODY = """<html>
 <head></head>
 <body>
 <marquee>hello!</marquee>
 </body>
 </html>"""
-response = """HTTP/1.1 200 OK
-Content-Length: {1}
+RESPONSE_HEAD = """HTTP/1.1 200 OK
+Content-Length: {0}
 Connection: close
+""".format(len(RESPONSE_BODY))
 
-{0}""".format(responseBody, len(responseBody))
+class Responder:
+    def process(self, request):
+        return Response(RESPONSE_HEAD, RESPONSE_BODY)
+
+class Router:
+    def findResponder(self, request):
+        return {
+            "/": Responder()
+        }[request.route()]
+
+class Request:
+    def __init__(self, text):
+        self.text = text
+    def __str__(self):
+        return self.text
+    def route(self):
+        return "/"
+    def method(self):
+        return "GET"
+
+class Response:
+    def __init__(self, head, body):
+        self.head = head
+        self.body = body
+    def __str__(self):
+        return self.head + "\n" + self.body
+    def code(self):
+        return "200"
 
 class Server:
-    def __init__(self, host, port):
+    def __init__(self, host, port, router):
         self.host = host
         self.port = port
+        self.router = router
 
-    def listen(self):
+    def run(self):
         with self.bindSocket() as s:
+            s.listen()
             while True:
-                s.listen()
-                conn, addr = s.accept()
-                with conn:
-                    print('Connected by', addr)
-                    self.respond(conn)
-                    conn.close()
+                self.acceptConnection(s)
 
     def bindSocket(self):
         theSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         theSocket.bind((self.host, self.port))
         return theSocket
 
-    def respond(self, conn):
-        data = conn.recv(2048)
-        conn.sendall(bytearray(response, "UTF-8"))
+    def acceptConnection(self, rootSocket):
+        connection, address = rootSocket.accept()
+        with connection:
+            print('# incoming connection from: ', address)
+            self.processConnection(connection)
+            connection.close()
 
+    def processConnection(self, connection):
+        request = self.parseRequest(connection)
+        response = self.generateResponse(request)
+        print('## request: ', request)
+        print('## response: ', response)
+        self.sendResponse(response, connection)
+        connection.close()
 
-Server(HOST, PORT).listen()
+    def parseRequest(self, conn):
+        data = conn.recv(8)
+        return Request(str(data))
 
+    def generateResponse(self, request):
+        responder = self.router.findResponder(request)
+        response = responder.process(request)
+        return response
+
+    def sendResponse(self, response, connection):
+        connection.sendall(bytearray(str(response), "UTF-8"))
+
+Server(HOST, PORT, Router()).run()
 
