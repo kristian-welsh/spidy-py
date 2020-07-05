4542d2720d69e2c1132e425cfd81cc3b267a02f8
Kristian Welsh <kriswelsh@gmail.com>
2020-07-05 20:27:14 +0100

    accept multiple subsequent connections

--- server.py
+++ server.py
@@ -10,6 +10,7 @@ responseBody = """<html>
 </html>"""
 response = """HTTP/1.1 200 OK
 Content-Length: {1}
+Connection: close
 
 {0}""".format(responseBody, len(responseBody))
 
@@ -19,17 +20,24 @@ class Server:
         self.port = port
 
     def listen(self):
-        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-            s.bind((self.host, self.port))
-            s.listen()
-            conn, addr = s.accept()
-            with conn:
-                print('Connected by', addr)
-                while True:
-                    data = conn.recv(2048)
-                    if not data:
-                        break;
-                    conn.sendall(bytearray(response, "UTF-8"))
+        with self.bindSocket() as s:
+            while True:
+                s.listen()
+                conn, addr = s.accept()
+                with conn:
+                    print('Connected by', addr)
+                    self.respond(conn)
+                    conn.close()
+
+    def bindSocket(self):
+        theSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        theSocket.bind((self.host, self.port))
+        return theSocket
+
+    def respond(self, conn):
+        data = conn.recv(2048)
+        conn.sendall(bytearray(response, "UTF-8"))
+
 
 Server(HOST, PORT).listen()
 
